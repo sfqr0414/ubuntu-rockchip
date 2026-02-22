135ad21ae565ecd0c67680e28454d671e71bcf4f
sfqr0414 <sfqr0414@gmail.com>
2026-02-22 09:15:40 +0800

    Uncomment COMPATIBLE_FLAVORS in orangepi-5-max.sh

--- config/boards/orangepi-5-max.sh
+++ config/boards/orangepi-5-max.sh
@@ -8,7 +8,7 @@ export UBOOT_PACKAGE="u-boot-radxa-rk3588"
 export UBOOT_RULES_TARGET="orangepi-5-max-rk3588"
 #export COMPATIBLE_SUITES=("jammy" "noble" "oracular" "plucky")
 export COMPATIBLE_SUITES=("plucky")
-#export COMPATIBLE_FLAVORS=("server" "desktop")
+export COMPATIBLE_FLAVORS=("server" "desktop")
 
 function config_image_hook__orangepi-5-max() {
     local rootfs="$1"
