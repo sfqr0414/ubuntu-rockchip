98f16cff707d73471abc1a60c5bba799d8115053
sfqr0414 <sfqr0414@gmail.com>
2026-03-03 18:28:44 +0800

    Change compatible suites and add multimedia packages
    Updated the compatible suites for the orangepi-5-max configuration and added multimedia package installations.

--- config/boards/orangepi-5-max.sh
+++ config/boards/orangepi-5-max.sh
@@ -7,7 +7,7 @@ export BOARD_CPU="ARM Cortex A76 / A55"
 export UBOOT_PACKAGE="u-boot-radxa-rk3588"
 export UBOOT_RULES_TARGET="orangepi-5-max-rk3588"
 #export COMPATIBLE_SUITES=("jammy" "noble" "oracular" "plucky")
-export COMPATIBLE_SUITES=("plucky")
+export COMPATIBLE_SUITES=("noble")
 export COMPATIBLE_FLAVORS=("server" "desktop")
 
 function config_image_hook__orangepi-5-max() {
@@ -16,33 +16,8 @@ function config_image_hook__orangepi-5-max() {
     local suite="$3"
 
     if [ "${suite}" == "jammy" ] || [ "${suite}" == "noble" ]; then
-        # Kernel modules to blacklist
-        echo "blacklist bcmdhd" > "${rootfs}/etc/modprobe.d/bcmdhd.conf"
-        echo "blacklist dhd_static_buf" >> "${rootfs}/etc/modprobe.d/bcmdhd.conf"
-
-        # Install panfork
-        chroot "${rootfs}" add-apt-repository -y ppa:jjriek/panfork-mesa
-        chroot "${rootfs}" apt-get update
-        chroot "${rootfs}" apt-get -y install mali-g610-firmware
-        chroot "${rootfs}" apt-get -y dist-upgrade
-
-        # Install libmali blobs alongside panfork
-        chroot "${rootfs}" apt-get -y install libmali-g610-x11
-
-        # Install the rockchip camera engine
-        chroot "${rootfs}" apt-get -y install camera-engine-rkaiq-rk3588
-
-        # Install BCMDHD SDIO WiFi and Bluetooth DKMS (do not print make.log here)
-        chroot "${rootfs}" apt-get -y install dkms bcmdhd-sdio-dkms
-
-        # Enable bluetooth
-        cp "${overlay}/usr/bin/brcm_patchram_plus" "${rootfs}/usr/bin/brcm_patchram_plus"
-        cp "${overlay}/usr/lib/systemd/system/ap6611s-bluetooth.service" "${rootfs}/usr/lib/systemd/system/ap6611s-bluetooth.service"
-        chroot "${rootfs}" systemctl enable ap6611s-bluetooth
-
-        # Install wiring orangepi package 
-        chroot "${rootfs}" apt-get -y install wiringpi-opi libwiringpi2-opi libwiringpi-opi-dev
-        echo "BOARD=orangepi5max" > "${rootfs}/etc/orangepi-release"
+        chroot "${rootfs}" add-apt-repository -y ppa:jjriek/rockchip-multimedia
+        chroot "${rootfs}" apt-get -y install librga mpp gstreamer1.0-rockchip ffmpeg
     else
         DOWNLOAD_URL="https://github.com/sfqr0414/test_action/releases/download/repo"
         DOWNLOAD_FILES=(#"armbian-firmware-gpu-panthor.deb" \
