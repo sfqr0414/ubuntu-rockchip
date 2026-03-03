627bfb697df7bc1cb55337b95a814851f1cce6a4
sfqr0414 <sfqr0414@gmail.com>
2026-03-03 18:34:53 +0800

    Create noble.sh

---
+++ config/suites/noble.sh
@@ -0,0 +1,10 @@
+# shellcheck shell=bash
+
+export RELEASE_NAME="Ubuntu 24.04 LTS (Noble Nombat)"
+export RELEASE_VERSION="24.04"
+
+export KERNEL_REPO="https://github.com/sfqr0414/linux-rockchip.git"
+export KERNEL_BRANCH="noble"
+export KERNEL_BRANCH="armbian-import"
+
+export EXTRA_PPAS="jjriek/rockchip jjriek/rockchip-multimedia"
