76214b650aa8af3afeb199a38317f2b1acfdaced
sfqr0414 <sfqr0414@gmail.com>
2026-03-08 00:34:28 +0800

    Update tweaks.sh

--- definitions/tweaks.sh
+++ definitions/tweaks.sh
@@ -52,6 +52,10 @@ EOF
 Package: firefox*
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
+
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1
 EOF
     apt update
     apt policy firefox
@@ -105,7 +109,7 @@ EOF
 
 {
     # 清理：必须在脚本结束前卸载，否则 ubuntu-image 拷贝会崩溃
-    host_call "umount -l /proc"roc; do
+    host_call "umount -l /proc"
     host_call "umount -l /sys"
     host_call "umount -l /dev/pts"
     # 别忘了删掉刚建的设备节点，防止拷贝工具报错
