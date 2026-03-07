234408a913bf819f60f97686cb1b7add5e5733f1
sfqr0414 <sfqr0414@gmail.com>
2026-03-07 10:17:31 +0800

    Update config-image.sh

--- scripts/config-image.sh
+++ scripts/config-image.sh
@@ -131,7 +131,7 @@ type configure_apt_sources &> /dev/null && "$_" "$chroot_dir" "${SUITE}"
 #configure_apt_sources "$chroot_dir" "${SUITE}"
 
 chroot $chroot_dir apt-get update -o Acquire::Max-Fetchers=16 -o Acquire::Max-Conns-Per-Host=8
-chroot "${rootfs}" apt-get dist-upgrade -y
+chroot $chroot_dir apt-get dist-upgrade -y
 #chroot $chroot_dir apt-get -y upgrade
 chroot $chroot_dir apt-get install -y --no-install-recommends software-properties-common ca-certificates gnupg2
 
